6c7ff734b1fdc2f6200253d634a25aa2ce1af8b1
Matheus Wilian Antunes <32783454+Teavan123@users.noreply.github.com>
2024-09-09 16:29:56 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Dota2",
+        descricao: "Dota 2 é um jogo multiplayer online battle arena (MOBA) que conquistou milhões de jogadores ao redor do mundo. Nele, duas equipes de cinco jogadores se enfrentam em mapas complexos, cada um controlando um poderoso herói com habilidades únicas.",
+        link: "https://www.dota2.com/home",
+        tags: "dota, Dota 2, moba, MOBA,jogo multiplayer online,mapas complexos"
+    },
+    {
+        titulo: "Sea of Thieves",
+        descricao: "Sea of Thieves é um jogo de aventura multiplayer no qual você navega pelos mares, procura tesouros e enfrenta outros piratas. Explore ilhas, enfrente tempestades e construa sua própria lenda pirata.",
+        link: "https://www.seaofthieves.com/",
+        tags:"jogo de barquinho, Sea, sea of thives,seaoftives,jogo de aventura,exploração,pirata"
+    },
+    {
+        titulo: "The Forest",
+        descricao: "The Forest é um jogo de terror de sobrevivência em mundo aberto. Após um acidente de avião, você acorda em uma floresta canibal. Lute pela sobrevivência, construa abrigos, fabrique ferramentas e explore a ilha misteriosa.",
+        link: "https://store.steampowered.com/app/242760/The_Forest/",
+        tags:"the forest,the,florestinha,jogo de terror de sobrevivência,terror,sobrevivência,mundo aberto"
+    }
+];
